04094634abe755f8d41051d96fbf6ba9ca275697
LuukyPuuky <147500632+LuukyPuuky@users.noreply.github.com>
2026-03-10 15:06:43 +0100

    install Nativewind to app.

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-green-500"></Text>
+    </View>
+  );
+}
